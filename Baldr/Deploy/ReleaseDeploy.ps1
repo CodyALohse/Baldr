3b7d18abe4f38b9a1d04a7938ff06dec0d06a3f7
Cody Lohse <luckylohse@gmail.com>
2017-04-20 22:02:48 -0500

    Updated ReleaseDeploy.ps1. Stops Baldr service if running, deploys app, and re-starts the service.

--- Baldr/Deploy/ReleaseDeploy.ps1
+++ Baldr/Deploy/ReleaseDeploy.ps1
@@ -8,9 +8,42 @@
 param(
     [string]$ProjectDirectory,
     [string]$PublishDirectory,
-    [string]$Configuration
+    [string]$Configuration,
+    [string]$ServiceName
 )
 
-cd $ProjectDirectory
-dotnet restore
-dotnet publish $ProjectDirectory -o $PublishDirectory -c $Configuration
+
+Function Publish-App{
+    Param(
+        [string]$ProjectDirectory,
+        [string]$PublishDirectory,
+        [string]$Configuration
+    )
+
+    Write-Host "Switching to project directory" + $ProjectDirectory -ForegroundColor Green
+    cd $ProjectDirectory
+
+    Write-Host "Starting Dotnet restore" -ForegroundColor Green
+    dotnet restore
+
+    Write-Host "Starting Dotnet publish" -ForegroundColor Green
+    dotnet publish $ProjectDirectory -o $PublishDirectory -c $Configuration
+
+    Write-Host "Dotnet publish finished" -ForegroundColor Green
+}
+
+
+Get-Service -DisplayName $ServiceName | Sort-Object Status, Displayname |
+ForEach-Object {
+    $Displayname = $_.DisplayName
+    If($_.Status -eq "Running")
+    {
+        Write-Host "Service is running - Stopping Service" -ForegroundColor Green
+        Stop-Service -DisplayName $Displayname
+    }
+
+    Publish-App -ProjectDirectory $ProjectDirectory -PublishDirectory $PublishDirectory -Configuration $Configuration
+
+    Write-Host "Starting service"
+    Start-Service -DisplayName $Displayname
+}
